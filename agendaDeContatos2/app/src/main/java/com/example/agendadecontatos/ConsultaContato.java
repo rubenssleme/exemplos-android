c5932f2e5f293277767674e43dc751f5df3a3d0f
rubenssleme <ajua614728>
2020-07-19 19:45:24 -0300

    Implementação da chamada a tela cadastro para novo cadastro apartir da consulta.

--- agendaDeContatos2/app/src/main/java/com/example/agendadecontatos/ConsultaContato.java
+++ agendaDeContatos2/app/src/main/java/com/example/agendadecontatos/ConsultaContato.java
@@ -2,7 +2,9 @@ package com.example.agendadecontatos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -18,12 +20,23 @@ public class ConsultaContato extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consulta_contato);
-        buttonNovoContato = findViewById(R.id.buttonAdicionarNovo);
+        buttonNovoContato = findViewById(R.id.buttonNovoContato);
         listViewContato = findViewById(R.id.listViewContato);
         DaoContato daoContato = new DaoContato(getApplicationContext());
 
         arrayListContato = daoContato.consultarTodos();
         ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(),android.R.layout.simple_list_item_1,arrayListContato);
         listViewContato.setAdapter(adapter);
+        cadastrarNovoContato();
+    }
+
+    private void cadastrarNovoContato(){
+        buttonNovoContato.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent novoContato = new Intent(getApplicationContext(),CadastrarContato.class);
+                startActivity(novoContato);
+            }
+        });
     }
 }
