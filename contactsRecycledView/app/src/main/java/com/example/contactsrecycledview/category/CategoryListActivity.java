e5eb7a5854e8caba81d121147a8c37ced170d1d1
Rubens Leme <rubenss.leme@gmail.com>
2021-08-22 20:48:44 -0300

    Criação das classes Category usando recycledview categoria.

--- contactsRecycledView/app/src/main/java/com/example/contactsrecycledview/category/CategoryListActivity.java
+++ contactsRecycledView/app/src/main/java/com/example/contactsrecycledview/category/CategoryListActivity.java
@@ -1,4 +1,21 @@
 package com.example.contactsrecycledview.category;
 
-public class CategoryListActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.contactsrecycledview.R;
+
+public class CategoryListActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // ...
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+
+        // Lookup the recyclerview in activity layout
+        RecyclerView rvContacts = (RecyclerView) findViewById(R.id.rcListaCategoria);
+
+    }
 }
